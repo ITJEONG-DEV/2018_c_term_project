9cc54af95fd120904d5beab61ae62a1f1e24647f
ITJEONG <derbana1027@gmail.com>
2018-11-15 15:26:50 +0900

    struct pointer array malloc

--- term.c
+++ term.c
@@ -11,8 +11,8 @@
 
 typedef struct
 {
-   char title[MAX_TITLE_LENGTH];
-   char content[MAX_TITLE_LENGTH];
+   char *title;
+   char *content;
 } _PLAN;
 
 typedef struct
@@ -28,20 +28,26 @@ typedef struct
 } _DAY;
 
 int isLeapYear(int year); //윤년인지 아닌지를 판단하는 함수
-int getDayoftheWeek(int year, int month, int day); // 년, 월, 일을 넘기면 요일을 반환하는 함수
-_DAY initTargetDay(int month_day[]); // 오늘 날짜를 받아와서 기준일을 생성하는 함수
+char * getDayoftheWeek(int year, int month, int day); // 년, 월, 일을 넘기면 요일을 반환하는 함수
+_DAY * initTargetDay(int monthDay[]); // 오늘 날짜를 받아와서 기준일을 생성하는 함수
+void displayDay(_DAY * targetDay);
 
 int main()
 {
-   int month_day[] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-   char dayoftheWeek[7][4] = { "SUN\0", "MON\0", "TUE\0", "WED\0", "THU\0", "FRI\0", "SAT\0" };
+   int monthDay[] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
-   _DAY targetDay = initTargetDay(month_day);
+   _DAY * targetDay = initTargetDay(monthDay);
 
-   printf("%d-%d-%d\n", targetDay.year, targetDay.month, targetDay.day);
+   char view_Type = 'D'; // Day Week Month
+   int onlyRegisteredSchedules = 0;
 
-   int answer = getDayoftheWeek(targetDay.year, targetDay.month, targetDay.day);
+   targetDay->plan = (_PLAN*)malloc(sizeof(_PLAN));
+   targetDay->plan->title = (char*)malloc(sizeof(char * MAX_TITLE_LENGTH));
+   targetDay->plan->title = "test title yeah";
+   targetDay->plan->content = (char*)malloc(sizeof(char * MAX_CONTENT_LENGTH));
+   targetDay->plan->content = "test content yeah";
 
+   displayDay(targetDay);
 }
 
 int isLeapYear(int year)
@@ -50,9 +56,10 @@ int isLeapYear(int year)
    else return 0;
 }
 
-int getDayoftheWeek(int year, int month, int day)
+char * getDayoftheWeek(int year, int month, int day)
 {
-   int month_day[] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+   int monthDay[] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+   char dayoftheWeek[7][4] = { "SUN\0", "MON\0", "TUE\0", "WED\0", "THU\0", "FRI\0", "SAT\0" };
 
    // 1900년 1월 1일은 월요일
    // 일월화수목금토 0 ~ 6
@@ -66,18 +73,21 @@ int getDayoftheWeek(int year, int month, int day)
       if( i == 2 && isLeapYear(year) )
          ++date;
 
-      date+=month_day[i];
+      date+=monthDay[i];
    }
 
    date+=day;
 
-   return date&7;
+   char * tmp = malloc(sizeof(char)*3);
 
+   strcpy( tmp, dayoftheWeek[date&7]);
+
+   return tmp;
 }
 
-_DAY initTargetDay(int month_day[])
+_DAY * initTargetDay(int monthDay[])
 {
-   _DAY targetDay;
+   _DAY * targetDay;
 
    int day;
 
@@ -86,15 +96,15 @@ _DAY initTargetDay(int month_day[])
       printf("년 월 일을 입력하세요. 입력 형태 : 20181105\n> ");
       scanf("%d", &day);
 
-      targetDay.year = day/10000;
-      targetDay.month = (day/100)%100;
-      if( targetDay.month < 1 || targetDay.month > 12 )
+      targetDay->year = day/10000;
+      targetDay->month = (day/100)%100;
+      if( targetDay->month < 1 || targetDay->month > 12 )
       {
          printf("[error]올바른 월을 입력하세요.\n");
          continue;
       }
-      targetDay.day = day%100;
-      if( targetDay.day < 1 || targetDay.day > month_day[targetDay.month] )
+      targetDay->day = day%100;
+      if( targetDay->day < 1 || targetDay->day > monthDay[targetDay->month] )
       {
          printf("[error]올바른 일을 입력하세요.\n");
          continue;
@@ -104,4 +114,20 @@ _DAY initTargetDay(int month_day[])
    }
 
    return targetDay;
+}
+
+void displayDay(_DAY * targetDay)
+{
+   printf("%4d-%2d-%2d ", targetDay->year, targetDay->month, targetDay->day);
+   printf("%s\n", getDayoftheWeek(targetDay->year, targetDay->month, targetDay->day));
+
+   int i = 0;
+
+   while( targetDay->plan )
+   {
+      printf("> %s\n", targetDay->plan->title );
+      printf("%s\n\n", targetDay->plan->content );
+
+      ++i;
+   }
 }
